249168321c66fb1c09991ea37eb4f3d0d6b28376
samirm00 <sammoham.be@gmail.com>
2021-03-12 15:49:23 +0100

    add routes

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,16 @@
-const controllers = require('./controllers.js');
-const express = require('express');
+const controllers = require("./controllers.js");
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', controllers.hello);
+router.get("/", controllers.hello);
 
 // write your routes
 
+router.get("/courses", controllers.getAllCourses);
+router.get("/courses/:id", controllers.getCourse);
+router.post("/courses", controllers.addCourse);
+router.put("/courses/:id", controllers.updateCourse);
+router.delete("/courses/:id", controllers.deleteCourse);
 
 module.exports = router;
